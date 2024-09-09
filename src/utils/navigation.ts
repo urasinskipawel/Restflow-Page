9de580f8f933a26b42922ed3862d2fe04005fc82
Pawel Urasinski <uraspawel@gmail.com>
2024-09-09 21:10:39 +0200

    Created a smoth scroll to each section

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,16 +1,17 @@
 // An array of links for navigation bar
 export const navigationData = [
-  { name: "Strona główna", url: "/" },
-  { name: "Funkcje", url: "/funkcje" },
-  { name: "Korzyści", url: "/korzysci" },
-  { name: "Plany", url: "/plany" },
-  { name: "Kontakt", url: "/kontakt" },
+  { name: "Strona główna", url: "#" },
+  { name: "Funkcje", url: "#about" },
+  { name: "Partnerzy", url: "#clients" },
+  { name: "Korzyści", url: "#features" },
+  { name: "Plany", url: "#pricing" },
+  { name: "Kontakt", url: "#contact" },
 ];
 // An object of links for social icons
 export const socialLinks = {
-  facebook: "https://www.facebook.com/",
-  x: "https://twitter.com/",
-  github: "https://github.com/mearashadowfax/ScrewFast",
-  google: "https://www.google.com/",
-  slack: "https://slack.com/",
+  facebook: "#",
+  x: "#",
+  github: "#",
+  google: "#",
+  slack: "#",
 };
