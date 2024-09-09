af6e7504d2bbc1dd3bb3011ec52c7c23708540f3
Pawel Urasinski <uraspawel@gmail.com>
2024-09-09 20:06:44 +0200

    Added temporary favicon. Changed description of website

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -1,12 +1,12 @@
 import ogImageSrc from "@images/social.png";
 
 export const SITE = {
-  title: "ScrewFast",
-  tagline: "Top-quality Hardware Tools",
-  description: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.",
-  description_short: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs.",
-  url: "https://screwfast.uk",
-  author: "Emil Gulamov",
+  title: "RestFlow",
+  tagline: "Zrewolucjonizuj zarządzanie swoją restauracją z Restflow",
+  description: "Restflow to idealne narzędzie dla właścicieli restauracji, szefów kuchni, menedżerów i zespołów zarządzających, którzy szukają nowoczesnego i efektywnego sposobu na zarządzanie codziennymi obowiązkami.",
+  description_short: "Restflow to idealne narzędzie do nowoczesnego i efektywnego sposobu na zarządzanie codziennymi obowiązkami.",
+  url: "",
+  author: "Paweł Urasiński",
 };
 
 export const SEO = {
@@ -34,6 +34,6 @@ export const OG = {
   type: "website",
   url: SITE.url,
   title: `${SITE.title}: : Hardware Tools & Construction Services`,
-  description: "Equip your projects with ScrewFast's top-quality hardware tools and expert construction services. Trusted by industry leaders, ScrewFast offers simplicity, affordability, and reliability. Experience the difference with user-centric design and cutting-edge tools. Start exploring now!",
+  description: "Kontroluj swoje zaopatrzenie, oszczędzaj czas, pieniądze i nerwy oraz nigdy nie przegap najniższych cen u swoich dostawców - to wszystko oraz więcej zaoferuje Ci RestFlow!",
   image: ogImageSrc,
 };
--- src/pages/favicon.ico.ts
+++ src/pages/favicon.ico.ts
@@ -3,20 +3,16 @@ import sharp from "sharp";
 import ico from "sharp-ico";
 import path from "node:path";
 
-const faviconSrc = path.resolve("src/images/icon.png");
+const faviconSrc = path.resolve("src/images/IconDA.png");
 
 export const GET: APIRoute = async () => {
-
   // Resize the image to multiple sizes
   const sizes = [16, 32];
 
   const buffers = await Promise.all(
     sizes.map(async (size) => {
-      return await sharp(faviconSrc)
-        .resize(size)
-        .toFormat("png")
-        .toBuffer();
-    })
+      return await sharp(faviconSrc).resize(size).toFormat("png").toBuffer();
+    }),
   );
 
   // Convert the image to an ICO file
