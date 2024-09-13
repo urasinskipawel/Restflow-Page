940e4d82bd53ffa6e56e7e76b29df9f8910b8a9b
Pawel Urasinski <uraspawel@gmail.com>
2024-09-14 00:04:52 +0200

    Created view to inform user after sending form

--- src/components/ui/forms/ContactForm.tsx
+++ src/components/ui/forms/ContactForm.tsx
@@ -1,12 +1,11 @@
+import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { TextInput } from "@components/ui/forms/input/TextInput";
 import { EmailContactInput } from "@components/ui/forms/input/EmailContactInput";
 import { PhoneInput } from "@components/ui/forms/input/PhoneInput";
 import { Checkbox } from "@components/ui/forms/input/Checkbox";
+import { FormProgression } from "./FormProgression";
 
-interface ContactFormProps {
-  formTitle: string;
-}
 interface ContactFormInputs {
   restaurantName: string;
   city: string;
@@ -16,7 +15,8 @@ interface ContactFormInputs {
   privacyPolicy: boolean;
 }
 
-export const ContactForm = ({ formTitle }: ContactFormProps) => {
+export const ContactForm = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false);
   const {
     handleSubmit,
     reset,
@@ -26,6 +26,7 @@ export const ContactForm = ({ formTitle }: ContactFormProps) => {
 
   const onSubmit = (data) => {
     console.log("Dane formularza:", data);
+    setIsSubmitted(true);
   };
 
   const handleClear = () => {
@@ -33,152 +34,167 @@ export const ContactForm = ({ formTitle }: ContactFormProps) => {
   };
 
   return (
-    <div className="pt-9">
-      <h2 className="flex text-balance pb-6 font-oswald text-3xl font-bold text-green-500">
-        {formTitle}
-      </h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex flex-col gap-5">
-          <div>
-            <Controller
-              name="restaurantName"
-              control={control}
-              defaultValue=""
-              rules={{ required: "To pole jest wymagane" }}
-              render={({ field }) => (
-                <>
-                  <TextInput {...field} placeholder="Nazwa restauracji" />
-                  {errors.restaurantName && (
-                    <span className="text-red-500">
-                      {errors.restaurantName.message}
-                    </span>
+    <>
+      <FormProgression isSubmitted={isSubmitted} />
+      <div className="pt-9">
+        <h2 className="flex text-balance pb-6 font-oswald text-3xl font-bold text-green-500">
+          {!isSubmitted ? "Dane kontaktowe" : "Dziękujemy"}
+        </h2>
+        {!isSubmitted && (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-5">
+              <div>
+                <Controller
+                  name="restaurantName"
+                  control={control}
+                  defaultValue=""
+                  rules={{ required: "To pole jest wymagane" }}
+                  render={({ field }) => (
+                    <>
+                      <TextInput {...field} placeholder="Nazwa restauracji" />
+                      {errors.restaurantName && (
+                        <span className="text-red-500">
+                          {errors.restaurantName.message}
+                        </span>
+                      )}
+                    </>
                   )}
-                </>
-              )}
-            />
-          </div>
-          <div>
-            <Controller
-              name="city"
-              control={control}
-              defaultValue=""
-              rules={{ required: "To pole jest wymagane" }}
-              render={({ field }) => (
-                <>
-                  <TextInput {...field} placeholder="Miejscowość" />
-                  {errors.city && (
-                    <span className="text-red-500">{errors.city.message}</span>
-                  )}
-                </>
-              )}
-            />
-          </div>
-          <div>
-            <Controller
-              name="fullName"
-              control={control}
-              defaultValue=""
-              rules={{ required: "To pole jest wymagane" }}
-              render={({ field }) => (
-                <>
-                  <TextInput {...field} placeholder="Imię i nazwisko" />
-                  {errors.fullName && (
-                    <span className="text-red-500">
-                      {errors.fullName.message}
-                    </span>
-                  )}
-                </>
-              )}
-            />
-          </div>
-          <div>
-            <Controller
-              name="email"
-              control={control}
-              defaultValue=""
-              rules={{
-                required: "To pole jest wymagane",
-                pattern: {
-                  value: /^\S+@\S+$/i,
-                  message: "Nieprawidłowy format email",
-                },
-              }}
-              render={({ field }) => (
-                <>
-                  <EmailContactInput {...field} placeholder="Adres e-mail" />
-                  {errors.email && (
-                    <span className="text-red-500">{errors.email.message}</span>
-                  )}
-                </>
-              )}
-            />
-          </div>
-          <div>
-            <Controller
-              name="phone"
-              control={control}
-              defaultValue=""
-              rules={{
-                required: "To pole jest wymagane",
-                pattern: {
-                  value: /^[0-9]+$/,
-                  message: "Numer telefonu może zawierać tylko cyfry",
-                },
-              }}
-              render={({ field }) => (
-                <>
-                  <PhoneInput {...field} placeholder="Numer telefonu" />
-                  {errors.phone && (
-                    <span className="text-red-500">{errors.phone.message}</span>
-                  )}
-                </>
-              )}
-            />
-          </div>
-        </div>
-        <div className="my-5">
-          <Controller
-            name="privacyPolicy"
-            control={control}
-            defaultValue={false}
-            rules={{
-              required: "Musisz zaakceptować politykę prywatności",
-              pattern: {
-                value: /^[0-9]+$/,
-                message: "Numer telefonu może zawierać tylko cyfry",
-              },
-            }}
-            render={({ field }) => (
-              <>
-                <Checkbox
-                  {...field}
-                  label="Zapoznałem się i zgadzam się z treścią polityki prywatności."
                 />
-                {errors.privacyPolicy && (
-                  <span className="text-red-500">
-                    {errors.privacyPolicy.message}
-                  </span>
+              </div>
+              <div>
+                <Controller
+                  name="city"
+                  control={control}
+                  defaultValue=""
+                  rules={{ required: "To pole jest wymagane" }}
+                  render={({ field }) => (
+                    <>
+                      <TextInput {...field} placeholder="Miejscowość" />
+                      {errors.city && (
+                        <span className="text-red-500">
+                          {errors.city.message}
+                        </span>
+                      )}
+                    </>
+                  )}
+                />
+              </div>
+              <div>
+                <Controller
+                  name="fullName"
+                  control={control}
+                  defaultValue=""
+                  rules={{ required: "To pole jest wymagane" }}
+                  render={({ field }) => (
+                    <>
+                      <TextInput {...field} placeholder="Imię i nazwisko" />
+                      {errors.fullName && (
+                        <span className="text-red-500">
+                          {errors.fullName.message}
+                        </span>
+                      )}
+                    </>
+                  )}
+                />
+              </div>
+              <div>
+                <Controller
+                  name="email"
+                  control={control}
+                  defaultValue=""
+                  rules={{
+                    required: "To pole jest wymagane",
+                    pattern: {
+                      value: /^\S+@\S+$/i,
+                      message: "Nieprawidłowy format email",
+                    },
+                  }}
+                  render={({ field }) => (
+                    <>
+                      <EmailContactInput
+                        {...field}
+                        placeholder="Adres e-mail"
+                      />
+                      {errors.email && (
+                        <span className="text-red-500">
+                          {errors.email.message}
+                        </span>
+                      )}
+                    </>
+                  )}
+                />
+              </div>
+              <div>
+                <Controller
+                  name="phone"
+                  control={control}
+                  defaultValue=""
+                  rules={{
+                    required: "To pole jest wymagane",
+                    pattern: {
+                      value: /^[0-9]+$/,
+                      message: "Numer telefonu może zawierać tylko cyfry",
+                    },
+                  }}
+                  render={({ field }) => (
+                    <>
+                      <PhoneInput {...field} placeholder="Numer telefonu" />
+                      {errors.phone && (
+                        <span className="text-red-500">
+                          {errors.phone.message}
+                        </span>
+                      )}
+                    </>
+                  )}
+                />
+              </div>
+            </div>
+            <div className="my-5">
+              <Controller
+                name="privacyPolicy"
+                control={control}
+                defaultValue={false}
+                rules={{
+                  required: "Musisz zaakceptować politykę prywatności",
+                  pattern: {
+                    value: /^[0-9]+$/,
+                    message: "Numer telefonu może zawierać tylko cyfry",
+                  },
+                }}
+                render={({ field }) => (
+                  <>
+                    <Checkbox
+                      {...field}
+                      label="Zapoznałem się i zgadzam się z treścią polityki prywatności."
+                      id='checkbox-id'
+                    />
+                    {errors.privacyPolicy && (
+                      <span className="text-red-500">
+                        {errors.privacyPolicy.message}
+                      </span>
+                    )}
+                  </>
                 )}
-              </>
-            )}
-          />
-        </div>
-        <div className="flex flex-row-reverse gap-4">
-          <button
-            type="submit"
-            className="w-32 rounded bg-green-500 text-white"
-          >
-            Zamów
-          </button>
-          <button
-            type="reset"
-            className="clear-btn w-32 rounded bg-green-50 p-2 text-green-500 shadow-md"
-            onClick={handleClear}
-          >
-            Wyczyść
-          </button>
-        </div>
-      </form>
-    </div>
+              />
+            </div>
+            <div className="flex flex-row-reverse gap-4">
+              <button
+                type="submit"
+                className="w-32 rounded bg-green-500 text-white"
+              >
+                Zamów
+              </button>
+              <button
+                type="reset"
+                className="clear-btn w-32 rounded bg-green-50 p-2 text-green-500 shadow-md"
+                onClick={handleClear}
+              >
+                Wyczyść
+              </button>
+            </div>
+          </form>
+        )}
+      </div>
+    </>
   );
 };
--- src/components/ui/forms/input/Checkbox.tsx
+++ src/components/ui/forms/input/Checkbox.tsx
@@ -1,13 +1,15 @@
 interface CheckboxProps {
+  id: string;
   label: string;
   onChange: () => void;
   onBlur: () => void;
 }
 
-export const Checkbox = ({ label, onChange, onBlur }: CheckboxProps) => (
+export const Checkbox = ({ id, label, onChange, onBlur }: CheckboxProps) => (
   <div className="flex items-center justify-center">
     <div className="p-4">
       <input
+        id={id}
         type="checkbox"
         className="h-4 w-4 appearance-none rounded-sm border-2 border-green-400 text-green-500 checked:border-green-500 checked:bg-green-500 focus:ring-0"
         onChange={onChange}
@@ -15,7 +17,7 @@ export const Checkbox = ({ label, onChange, onBlur }: CheckboxProps) => (
       />
     </div>
     <div className="px-3">
-      <label htmlFor={""} className="text-xs text-black">
+      <label htmlFor={id} className="text-xs text-black">
         {label} <slot />
       </label>
     </div>
